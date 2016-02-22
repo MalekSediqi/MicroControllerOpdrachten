13f7e35f651b26f26e070f909237ec98144636ce
MalekSediqi <maleksediqi17@gmail.com>
2016-02-22 22:19:11 +0100

    voor elke opdracht heb ik een aparte project gemaakt

---
+++ Week 2/Opdracht B2/Opdracht B2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht B2.c
+ *
+ * Created: 22-2-2016 22:07:57
+ * Author : malek
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Week 2/Opdracht B2/Opdracht B2/src/main.c
@@ -0,0 +1,78 @@
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+int ligt;
+
+/******************************************************************/
+
+
+/******************************************************************/
+ISR( INT0_vect )
+/*
+short:			ISR INT0
+inputs:
+outputs:
+notes:			Set PORTD.5
+Version :    	DMK, Initial code
+*******************************************************************/
+{
+	ligt = (ligt>>1);
+	PORTC = ligt;
+	if(ligt <= 0)
+	{
+		ligt = 256;
+		PORTC = ligt;
+	}
+}
+
+/******************************************************************/
+ISR( INT1_vect )
+/*
+short:			ISR INT1
+inputs:
+outputs:
+notes:			Clear PORTD.5
+Version :    	DMK, Initial code
+*******************************************************************/
+{
+	ligt = (ligt<<1);
+	PORTC = ligt;
+	if(ligt >= 255)
+	{
+		ligt = 1;
+		PORTC = ligt;
+	}
+	
+}
+
+/******************************************************************/
+int main( void )
+/*
+short:			main() loop, entry point of executable
+inputs:
+outputs:
+notes:			Slow background task after init ISR
+Version :    	DMK, Initial code
+*******************************************************************/
+{
+	
+	// Init I/O
+	DDRD = 0xF0;			// PORTD(7:4) output, PORTD(3:0) input
+	DDRC = 0xFF;
+	// Init Interrupt hardware
+	EICRA |= 0x0B;			// INT1 falling edge, INT0 rising edge
+	EIMSK |= 0x03;			// Enable INT1 & INT0
+	
+	// Enable global interrupt system
+	//SREG = 0x80;			// Of direct via SREG of via wrapper
+	sei();
+	PORTC = 1;
+	ligt = 1;
+	while (1)
+	{
+		
+	}
+
+	return 1;
+}
